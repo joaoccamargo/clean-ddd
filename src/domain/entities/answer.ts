4a87fccc7bab796559a63573a3a391f4526a002c
João Camargo <jclaudio00_12@hotmail.com>
2025-01-02 09:49:46 -0300

    adjusts and added types

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,13 +1,46 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/types/optional";
 
 interface AnswerProps {
-  content: string;
   authorId: string;
   questionId: string;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps>{
   get content(){
     return this.props.content;
+  }
+
+  set content(content: string){
+    this.props.content = content;
+  }
+
+  get authorId(){
+    return this.props.authorId;
+  }
+
+  get questionId(){
+    return this.props.questionId;
+  }
+
+  get createdAt(){
+    return this.props.createdAt;
+  }
+
+  get updatedAt(){
+    return this.props.updatedAt;
+  }
+
+  static create(props: Optional<AnswerProps, 'createdAt'>, id?: UniqueEntityID) {
+    const answer = new Answer({
+      ...props,
+      createdAt: new Date(),
+    }, id);
+  
+    return answer;
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,53 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/types/optional";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
-  slug: string;
-  authorId: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+  
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),
+    }, id);
+
+    return question;
+  }
 }
